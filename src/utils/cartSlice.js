8163090db376c7eddd79b8dc80416e063925b26d
Ashish B M <114829303+devAsh2@users.noreply.github.com>
2026-03-28 23:21:13 +0530

    Fix typo in initialState variable name

--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -6,7 +6,7 @@ const initialState = {
 
 const cartSlice = createSlice({
   name: 'cart',
-  initialStae,
+  initialState,
   reducers: {
     addToCart: (state, action) => {
       const product = action.payload;
